32f0db0a3447d465a1570cc6a9e47dd4afca09a5
Michael DeCicco <mdecicco8@gmail.com>
2024-11-14 01:18:17 -0500

    .

--- server/collection/index.js
+++ server/collection/index.js
@@ -55,7 +55,10 @@ class DataCollector {
         try {
             console.log('Scraping the spreadsheet...');
             this.progress({ current: 0, total: 1, percent: 0, status: 'Scraping spreadsheet' });
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                executablePath: '/usr/bin/chromium-browser',
+                args: ['--no-sandbox', '--disable-dev-shm-usage']
+            });
             const page = await browser.newPage();
             console.log('Puppeteer launched');
             page.on("pageerror", function (err) { console.log("Page error: " + err.toString()); });
